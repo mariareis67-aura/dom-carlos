33367bea8644aab91e8a4735b10541bc2d6175ca
mariareis67-aura <freitas.reis.maria@escola.pr.gov.br>
2026-04-06 10:45:34 -0300

    t

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const secaoagenda = document.querySelector("#agenda");
+    if (secaoagenda) {
+        const ulagenda = secaoagenda.querySelector("ul");
+        ulagenda.classList.add("agenda");
+    }
+
+}
+const secaocontato = document.querySelector("#contato");
+if(secaocontato){
+    secaocontato.classList.add(contato-box);
+    }
+
+});
